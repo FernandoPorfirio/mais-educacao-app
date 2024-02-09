7b0dc5915b2f6238fa322a293f9931f930cd92c0
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-02-09 07:53:36 -0300

    feat: add improvement to the component aesthetics

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -24,20 +24,21 @@ export default {
 </script>
 
 <template>
-  <div>
-    <app-bar @toggle-drawer="toggleDrawer" />
-    <v-navigation-drawer app v-model="drawer">
-      <v-list>
-        <v-list-item v-for="(link, index) in links" :key="index" :to="link.to">
-          <template v-slot:prepend>
-            <v-icon :icon="link.icon"></v-icon>
-          </template>
-          <v-list-item-title>{{ link.text }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-main class="d-flex align-center justify-center" style="min-height: 300px">
-      <router-view></router-view>
-    </v-main>
-  </div>
+  <v-navigation-drawer v-model="drawer">
+    <v-list>
+      <v-list-subheader>
+        Menu
+      </v-list-subheader>
+      <v-list-item v-for="(link, index) in links" :key="index" :to="link.to">
+        <template v-slot:prepend>
+          <v-icon :icon="link.icon"></v-icon>
+        </template>
+        <v-list-item-title>{{ link.text }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+  <app-bar @toggle-drawer="toggleDrawer" />
+  <v-main>
+    <router-view></router-view>
+  </v-main>
 </template>
