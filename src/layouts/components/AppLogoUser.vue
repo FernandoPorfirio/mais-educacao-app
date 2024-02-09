9c6c5f6da381f3ba5e38553dc3d22170f58b2f1f
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-02-09 07:33:29 -0300

    wik: add logo user componenet

---
+++ src/layouts/components/AppLogoUser.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-img :src="logoPath" alt="App logo user"></v-img>
+</template>
+
+<script>
+export default {
+  name: 'AppLogoUser',
+  data() {
+    return {
+      logoPath: null
+    }
+  },
+  async created() {
+    await this.loadLogo()
+  },
+  methods: {
+    async loadLogo() {
+      this.logoPath = (await import('@/assets/image/user_test.jpeg')).default
+    }
+  }
+}
+</script>
